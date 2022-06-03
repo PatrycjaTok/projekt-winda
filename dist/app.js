b51efe1edd67a72936ae8ff95850c8577a2511c6
PatrycjaTok <patrycjasuper999@gmail.com>
2022-06-03 20:38:06 +0200

    add input: first and last floor, cerated buttons for Elevators

--- dist/app.js
+++ dist/app.js
@@ -1,32 +1,81 @@
 const howManyElevators = document.querySelector("#howManyElevators");
+const floor1 = document.querySelector("#floor1");
+const floor2 = document.querySelector("#floor2");
 const confirmButton = document.querySelector("#confirmButton");
 const box1Div = document.querySelector("#box1");
-let doSymulation = false;
+let ElevatorsArrayOfObjects;
+class CreatedElevator {
+    constructor(id) {
+        this.id = id;
+        this.floorCurrent = 1;
+        this.floorTarget = 0;
+    }
+    ;
+}
+//const pickup:any = (currentFloor, floorTarget) =>{
+// console.log(`${currentFloor}, ${floorTarget}`);
+//};
+const printElevators = (floor1, floor2) => {
+    var j = 0;
+    ElevatorsArrayOfObjects.forEach(element => {
+        let currentFloor = element.floorCurrent;
+        j = j + 1;
+        box1Div.innerHTML += "<br>";
+        let div = document.createElement("div");
+        box1Div.appendChild(div);
+        div.innerHTML += `Winda-${j} <br>`;
+        for (var i = floor2; i >= floor1; i--) {
+            let button = document.createElement("button");
+            button.setAttribute("id", `buttonElevator${j}Floor${i}`);
+            button.setAttribute("class", `buttonElevator${j}`);
+            button.innerHTML = `${i}`;
+            div.appendChild(button);
+            //button.addEventListener("click", pickup(currentFloor, i));
+        }
+    });
+};
+const createElevators = (howManyElevators, floor1, floor2) => {
+    ElevatorsArrayOfObjects = [];
+    for (var i = 1; i <= howManyElevators; i++) {
+        let elevator = new CreatedElevator(`Elevator-${i}`);
+        ElevatorsArrayOfObjects.push(elevator);
+    }
+    printElevators(floor1, floor2);
+};
 confirmButton.addEventListener("click", (event) => {
     event.preventDefault();
     box1Div.textContent = "";
-    const howManyElevatorsValue = Number(howManyElevators.value);
+    let howManyElevatorsValue = Number(howManyElevators.value);
+    let floor1Value = Number(floor1.value);
+    let floor2Value = Number(floor2.value);
     if (howManyElevatorsValue % 1 === 0) {
         if (howManyElevatorsValue === 0) {
-            box1Div.textContent = "wprowadź ilość wind";
+            box1Div.textContent = "Wprowadź ilość wind";
         }
         else if (howManyElevatorsValue < 0) {
-            box1Div.textContent = "ujemna ilość wind - uciekły nam jakieś? :)";
+            box1Div.textContent = "Ujemna ilość wind - uciekły nam jakieś? :)";
         }
         else if (howManyElevatorsValue > 16) {
-            box1Div.textContent = "system może obsługiwać do 16 wind";
+            box1Div.textContent = "System może obsługiwać do 16 wind";
         }
         else {
-            doSymulation = true;
-            elevatorSystem(howManyElevatorsValue, doSymulation);
+            if ((floor1Value % 1 === 0) && (floor2Value % 1 === 0)) {
+                if (floor1Value < floor2Value) {
+                    createElevators(howManyElevatorsValue, floor1Value, floor2Value);
+                    howManyElevators.value = null;
+                    floor1.value = null;
+                    floor2.value = null;
+                }
+                else {
+                    box1Div.textContent = "Wprowadź poprawnie numery pięter";
+                }
+            }
+            else {
+                box1Div.textContent = "Najniższe i najwyższe piętro musi być liczbą całkowitą";
+            }
         }
     }
     else {
-        box1Div.textContent = "wprowadź liczbę całkowitą";
+        box1Div.textContent = "Wprowadź liczbę całkowitą";
     }
 });
-const elevatorSystem = (howManyElevators, doSymulation) => {
-    if (doSymulation === true) {
-        console.log("co dalej");
-    }
-};
--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,97 @@
 const howManyElevators: HTMLInputElement = document.querySelector("#howManyElevators");
+const floor1: HTMLInputElement = document.querySelector("#floor1");
+const floor2: HTMLInputElement = document.querySelector("#floor2");
 const confirmButton: HTMLButtonElement = document.querySelector("#confirmButton");
 const box1Div: HTMLElement = document.querySelector("#box1");
-let doSymulation: boolean = false;
+let ElevatorsArrayOfObjects: object[];
+
+class CreatedElevator {
+    id: string;
+    floorCurrent: number;
+    floorTarget: number;
+    
+    constructor(id: string){
+        this.id= id;
+        this.floorCurrent= 1;
+        this.floorTarget= 0;
+    }; 
+
+}
+
+//const pickup:any = (currentFloor, floorTarget) =>{
+   // console.log(`${currentFloor}, ${floorTarget}`);
+//};
+
+const printElevators = (floor1, floor2) =>{
+    var j: number = 0;
+    ElevatorsArrayOfObjects.forEach( element => {
+        let currentFloor:number = element.floorCurrent;
+        j=j+1;
+        box1Div.innerHTML+="<br>"
+        let div=document.createElement("div");
+        box1Div.appendChild(div);
+        div.innerHTML+=`Winda-${j} <br>`
+        for(var i=floor2; i>=floor1; i--){
+            let button = document.createElement("button");
+           button.setAttribute("id", `buttonElevator${j}Floor${i}`);
+           button.setAttribute("class", `buttonElevator${j}`);
+           button.innerHTML=`${i}`;
+           div.appendChild(button);
+            //button.addEventListener("click", pickup(currentFloor, i));
+        }
+    })
+}
+
+const createElevators = (howManyElevators: number, floor1: number, floor2: number) => {
+    ElevatorsArrayOfObjects = [];
+
+    for(var i=1; i<=howManyElevators; i++){
+        let elevator: object = new CreatedElevator(`Elevator-${i}`);
+        ElevatorsArrayOfObjects.push(elevator);
+    }
+    printElevators(floor1, floor2);
+
+};
+
+
 
 confirmButton.addEventListener("click",(event: Event)=>{
     event.preventDefault();
     box1Div.textContent="";
-    const howManyElevatorsValue: number =Number(howManyElevators.value);
+    let howManyElevatorsValue: number =Number(howManyElevators.value);
+    let floor1Value: number =Number(floor1.value);
+    let floor2Value: number =Number(floor2.value);
 
     if(howManyElevatorsValue%1===0){
         if(howManyElevatorsValue ===0){
-            box1Div.textContent="wprowadź ilość wind"
+            box1Div.textContent="Wprowadź ilość wind"
         }else if(howManyElevatorsValue <0){
-            box1Div.textContent="ujemna ilość wind - uciekły nam jakieś? :)"
+            box1Div.textContent="Ujemna ilość wind - uciekły nam jakieś? :)"
         }else if(howManyElevatorsValue >16){
-            box1Div.textContent="system może obsługiwać do 16 wind"
+            box1Div.textContent="System może obsługiwać do 16 wind"
         }else{
-            doSymulation=true;
-            elevatorSystem(howManyElevatorsValue, doSymulation);
+
+            if((floor1Value%1===0) && (floor2Value%1===0)){
+                if(floor1Value<floor2Value){
+                    createElevators(howManyElevatorsValue, floor1Value, floor2Value);
+                    howManyElevators.value=null;
+                    floor1.value=null;
+                    floor2.value=null;
+                }else{
+                    box1Div.textContent = "Wprowadź poprawnie numery pięter";
+                }
+            }else{
+                box1Div.textContent = "Najniższe i najwyższe piętro musi być liczbą całkowitą";
+            }
+
+            
         }
 
     }else{
-        box1Div.textContent = "wprowadź liczbę całkowitą";
+        box1Div.textContent = "Wprowadź liczbę całkowitą";
     }
 
 });
  
-const elevatorSystem = (howManyElevators: number, doSymulation: boolean) => {
-    if(doSymulation===true){
-        console.log("co dalej");
-    }
-}
+
+
