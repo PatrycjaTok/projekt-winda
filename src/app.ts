ceb6a2605b0079a7b75f264300feb6504079e177
PatrycjaTok <patrycjasuper999@gmail.com>
2022-06-06 23:11:12 +0200

    update files

--- src/app.ts
+++ src/app.ts
@@ -3,57 +3,40 @@ const floor1: HTMLInputElement = document.querySelector("#floor1");
 const floor2: HTMLInputElement = document.querySelector("#floor2");
 const confirmButton: HTMLButtonElement = document.querySelector("#confirmButton");
 const box1Div: HTMLElement = document.querySelector("#box1");
-let ElevatorsArrayOfObjects: object[];
+const btnToggleStatus: HTMLButtonElement = document.querySelector("#btnShowStatus");
+const divStatus: HTMLButtonElement = document.querySelector("#divStatus");
+const divbox2: HTMLButtonElement = document.querySelector("#box2");
+let ElevatorsArrayOfObjects: any[]=[];
+
+btnToggleStatus.addEventListener("click",()=>{divStatus.classList.toggle("visible");});
 
 class CreatedElevator {
-    id: string;
+    id: number;
     floorCurrent: number;
     floorTarget: number;
+    queuedirectionAndFrom: CreateQueueDirectionAndFrom[];
     
-    constructor(id: string){
+    constructor(id: number){
         this.id= id;
-        this.floorCurrent= 1;
-        this.floorTarget= 0;
+        this.floorCurrent= 0;
+        this.floorTarget= null;
+        this.queuedirectionAndFrom = [];
     }; 
-
 }
 
-//const pickup:any = (currentFloor, floorTarget) =>{
-   // console.log(`${currentFloor}, ${floorTarget}`);
-//};
-
-const printElevators = (floor1, floor2) =>{
-    var j: number = 0;
-    ElevatorsArrayOfObjects.forEach( element => {
-        let currentFloor:number = element.floorCurrent;
-        j=j+1;
-        box1Div.innerHTML+="<br>"
-        let div=document.createElement("div");
-        box1Div.appendChild(div);
-        div.innerHTML+=`Winda-${j} <br>`
-        for(var i=floor2; i>=floor1; i--){
-            let button = document.createElement("button");
-           button.setAttribute("id", `buttonElevator${j}Floor${i}`);
-           button.setAttribute("class", `buttonElevator${j}`);
-           button.innerHTML=`${i}`;
-           div.appendChild(button);
-            //button.addEventListener("click", pickup(currentFloor, i));
-        }
-    })
+class CreateQueueDirectionAndFrom {
+    floor: number;
+    upDown: string;
+    
+    constructor(floor, upDown){
+        this.floor= floor,
+        this.upDown= upDown;
+    }; 
 }
 
-const createElevators = (howManyElevators: number, floor1: number, floor2: number) => {
-    ElevatorsArrayOfObjects = [];
-
-    for(var i=1; i<=howManyElevators; i++){
-        let elevator: object = new CreatedElevator(`Elevator-${i}`);
-        ElevatorsArrayOfObjects.push(elevator);
-    }
-    printElevators(floor1, floor2);
-
+let changeShowInput = (elShow, val) =>{
+    elShow.setAttribute("value", `${val}`);
 };
-
-
 
 confirmButton.addEventListener("click",(event: Event)=>{
     event.preventDefault();
@@ -77,6 +60,11 @@ confirmButton.addEventListener("click",(event: Event)=>{
                     howManyElevators.value=null;
                     floor1.value=null;
                     floor2.value=null;
+ 
+                    setInterval( ()=>{     
+                        step();
+                    }, 1000); 
+
                 }else{
                     box1Div.textContent = "Wprowadź poprawnie numery pięter";
                 }
@@ -92,6 +80,163 @@ confirmButton.addEventListener("click",(event: Event)=>{
     }
 
 });
- 
+
+const createElevators = (howManyElevators: number, floor1: number, floor2: number) => {
+    ElevatorsArrayOfObjects = [];
+
+    for(var i=1; i<=howManyElevators; i++){
+        let elevator: object = new CreatedElevator(i);
+        ElevatorsArrayOfObjects.push(elevator);
+    };
+
+    printElevatorsButtons(floor1, floor2);
+};
+
+const printElevatorsButtons = (floor1: number, floor2: number) =>{    
+    var j: number = 0;
+    ElevatorsArrayOfObjects.forEach(element => {
+        divStatus.innerHTML+=`id windy: ${element.id}
+        | Obecne piętro: ${element.floorCurrent} 
+        | Docelowe piętro: ${element.floorTarget} <br>`;
+    });
+
+    ElevatorsArrayOfObjects.forEach(element => {
+        let objectElevator: object=element;
+        j=j+1;
+        let div4: HTMLDivElement=document.createElement("div");
+        box1Div.appendChild(div4);
+        div4.innerHTML+="<br>";
+        div4.innerHTML+=`Winda-${j} <br>`;
+        //let DivShowFloor: HTMLDivElement = document.createElement("div");
+        //DivShowFloor.setAttribute("id", `divShowFloorEl${j}`);
+        //DivShowFloor.setAttribute("class", `divShowFloorElevator${j}`);
+        //DivShowFloor.style.width = "25px";
+        //DivShowFloor.style.height = "20px";
+        //div4.appendChild(DivShowFloor);
+
+        let div: HTMLDivElement=document.createElement("div");
+        box1Div.appendChild(div);
+
+        let div2: HTMLDivElement=document.createElement("div");
+        div.appendChild(div2);
+        for(var i=floor2; i>=floor1; i--){
+            let numI: number = i;
+            let button: HTMLButtonElement = document.createElement("button");
+            button.setAttribute("id", `buttonElevator${j}Floor${numI}`);
+            button.setAttribute("class", `buttonElevator${j}`);
+            button.setAttribute("value", `${numI}`);
+            button.style.background = "yellow";
+            button.innerHTML=`${numI}`;
+            button.addEventListener("click", ()=>{ 
+                let upDownDirection: string;
+                if(element.floorCurrent < numI){
+                    upDownDirection="up";
+                }else{
+                    upDownDirection="down";
+                }
+                let FromWhereAndDirection: object = new CreateQueueDirectionAndFrom(numI,upDownDirection);
+                element.queuedirectionAndFrom.push(FromWhereAndDirection);
+                update(objectElevator);
+                console.log(ElevatorsArrayOfObjects);
+            })
+            div2.appendChild(button);
+        }
+   
+        let div3: HTMLDivElement=document.createElement("div");
+        div.appendChild(div3);
+        for(var i=floor2; i>=floor1; i--){
+            let numI: number = i;
+
+            let buttonUp: HTMLButtonElement= document.createElement("button");
+            buttonUp.setAttribute("id", `buttonUpElevator${numI}floor${numI}`);
+            buttonUp.setAttribute("class", `buttonUp`);
+            buttonUp.setAttribute("value", `up`);
+            buttonUp.style.background = "yellow";
+            buttonUp.innerHTML=`w górę`;
+            buttonUp.addEventListener("click", ()=>{                      // pickup, update queue
+                let FromWhereAndDirection: object = new CreateQueueDirectionAndFrom(numI,buttonUp.value);
+                element.queuedirectionAndFrom.push(FromWhereAndDirection);
+                update(objectElevator);
+            })
+            div3.appendChild(buttonUp);
+
+            let buttonDown: HTMLButtonElement= document.createElement("button");
+            buttonDown.setAttribute("id", `buttonDownElevator${j}floor${numI}`);
+            buttonDown.setAttribute("class", `buttonDown`);
+            buttonDown.setAttribute("value", `down`);
+            buttonDown.style.background = "orange";
+            buttonDown.innerHTML=`w dół`;
+            buttonDown.addEventListener("click", ()=>{                        // pickup, update queue
+                let FromWhereAndDirection: object = new CreateQueueDirectionAndFrom(numI,buttonDown.value);
+                element.queuedirectionAndFrom.push(FromWhereAndDirection); 
+                update(objectElevator);
+            })
+            div3.appendChild(buttonDown);
+
+        }
 
 
+    });
+};
+
+const update = (objectElevator) =>{
+    // //button.style.background = "green";
+
+    
+    //     for(let i=0; i<Number(objectElevator.queueWhereArray.length); i++){
+    //         if(Number(objectElevator.queueWhereArray[i])>=Number(objectElevator.floorCurrent)){
+    //             objectElevator.floorTarget=objectElevator.queueWhereArray[i];
+    //             step(Number(objectElevator.floorCurrent), Number(objectElevator.floorTarget));
+    //         };
+
+    //     };
+    
+};
+
+let optionalStop=(element: CreatedElevator, direction: string)=>{
+    let elementToRemove: CreateQueueDirectionAndFrom[]=[];
+    element.queuedirectionAndFrom.forEach(el => {
+        if(element.floorCurrent===el.floor && el.upDown === direction){
+            elementToRemove.push(el);
+        } 
+    });
+    elementToRemove.forEach(elRemove => {
+        element.queuedirectionAndFrom = element.queuedirectionAndFrom.filter(obj => {return obj !== elRemove});
+        console.log("ding!");
+    });
+};
+
+const step = () => {
+    ElevatorsArrayOfObjects.forEach(element => {
+
+        if((element.queuedirectionAndFrom.length>0) && (element.floorTarget===null)){
+            element.floorTarget=element.queuedirectionAndFrom.shift().floor;
+        }
+        if(element.floorTarget!==null){
+            if(element.floorCurrent < element.floorTarget){
+                element.floorCurrent+=1;
+                console.log(element.floorCurrent);
+                optionalStop(element, "up")
+            }else if(element.floorCurrent > element.floorTarget){
+                element.floorCurrent-=1;
+                console.log(element.floorCurrent);
+                optionalStop(element, "down")
+            }
+            console.log(ElevatorsArrayOfObjects);
+            if(element.floorCurrent === element.floorTarget){
+                element.floorTarget=null;
+                console.log("ding!");
+            }
+        }
+        updateUI(element);
+    });      
+};
+
+let updateUI = (element: CreatedElevator) => {
+    let buttonsAll: NodeListOf<HTMLButtonElement> = document.querySelectorAll(`.buttonElevator${element.id}`);
+    buttonsAll.forEach(buttonElement => {
+        buttonElement.style.background = "yellow";
+    });
+    let button: HTMLButtonElement=document.querySelector(`#buttonElevator${element.id}Floor${element.floorCurrent}`);
+    button.style.background = "green";
+}
